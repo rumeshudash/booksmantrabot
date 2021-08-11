8d826b1d60ecceff3e7269f4479c8695381d4e9a
Rumesh Udash <rumesh.udas@gmail.com>
2021-08-11 19:13:44 +0545

    Redoploy

--- index.js
+++ index.js
@@ -42,5 +42,6 @@ bot.on(MessengerPlatform.Events.MESSAGE, function(userId, message) {
 if (process.env.NODE_ENV !== 'production') {
     server.listen(port, () => console.log('Server is listening on port ' + port));
 } else {
+    
     server.listen(()=> console.log("Started Production"));
 }
